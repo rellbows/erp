e32332e33e5ba734e5c9d9cef49c66275e775ae1
bainiwaj <43765015+bainiwaj@users.noreply.github.com>
2019-10-20 12:36:11 -0700

    Update sample_data.sql

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -2,8 +2,9 @@ INSERT INTO `department` VALUES (1,'Computer Science', 'Kelley Engineering Cente
 (2,'Information Service', 'Milne Computer Center', 'Corvallis', 'OR'), (3,'English', 'Bexell', 'Corvallis', 'OR'),
 (4,'Physics', 'Weniger Hall', 'Corvallis', 'OR');
 
-INSERT INTO `user` (`user_id`, `user_type`, `first_name`, `last_name`, `signature_image_path`, `department_id`) VALUES (1, 'USER', 'Auston', 'Matthews', 'NULL', 4), (2, 'USER', 'William', 'Nylander', 'NULL', 3),
-(3, 'USER', 'Elias', 'Pettersson', 'NULL', 1), (4, 'ADMIN', 'Brent', 'Burns', 'NULL', 2);
+INSERT INTO `user` (`user_id`, `user_type`, `email`, `password`, `first_name`, `last_name`, `signature_image_path`, `department_id`) 
+VALUES (1, 'USER', '1@gmail.com', '1234', 'Auston', 'Matthews', 'NULL', 4), (2, 'USER', '2@gmail.com', 'QWERTY','William', 'Nylander', 'NULL', 3), (3, 'USER', '3@gmail.com', 'pass', 'Elias', 'Pettersson', 'NULL', 1), 
+(4, 'ADMIN', 'admin@gmail.com', '123', 'Brent', 'Burns', 'NULL', 2);
 
 INSERT INTO `awards` (`award_id`, `message`, `award_type`, `awardee_id`, `awarded_id`)
 VALUES (1, 'Thanks bro', 'Employee of the week', 1, 2), (2, 'Denied kid', 'Employee of the month', 4, 3);
