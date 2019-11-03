726bf08af744c42468424c470fd52fc8a83d1041
mohamesa <mohamesa@oregonstate.edu>
2019-11-03 10:30:56 -0600

    Add files via upload

---
+++ logout.js
@@ -0,0 +1,12 @@
+module.exports = function(){
+    	var express = require('express');
+    	var router = express.Router();
+	
+router.get('/', function(req, res){
+	//var context = {};
+//	res.render('logout', context);
+res.redirect('/login')
+});
+
+return router;
+} ();
